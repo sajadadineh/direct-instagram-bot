b745c888ae2ce3236f834f0f3276e8231cfc8e21
sajadadineh <sajadadineh79@gmail.com>
2020-03-10 18:20:47 +0330

    make login in instagram

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
-import constants
 import pickle
 import os
+import constants
+import userpass
 
 mobile_emulation = {"deviceName": "Nexus 5"}
 option = webdriver.ChromeOptions()
@@ -12,3 +13,11 @@ option.add_experimental_option("mobileEmulation", mobile_emulation)
 driver = webdriver.Chrome(executable_path='./chromedriver',chrome_options= option)
 driver.get(constants.base_url)
 
+def login():
+    driver.implicitly_wait(20)
+    driver.find_element_by_xpath(constants.login_button).click()
+    driver.find_element_by_xpath(constants.username).send_keys(userpass.username)
+    driver.find_element_by_xpath(constants.password).send_keys(userpass.password, Keys.ENTER)
+    sleep(2)
+
+login()
--- constants.py
+++ constants.py
@@ -1,1 +1,6 @@
 base_url = "https://www.instagram.com/"
+
+login_button = "//button[@class='sqdOP  L3NKy   y3zKF     ']"
+
+username = "//input[@name='username']"
+password = "//input[@name='password']"
